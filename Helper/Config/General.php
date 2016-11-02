fd382f39ee21ebd511555218e08890194ac61d24
Kenneth Bradley-White <ken@edmondscommerce.co.uk>
2016-11-02 14:46:52 +0000

    Adds currency-setting methods

--- Helper/Config/General.php
+++ Helper/Config/General.php
@@ -6,4 +6,20 @@ class General extends AbstractConfig
     {
         $this->setConfigPath('general/store_information/name', $name, $type, $scopeId);
     }
+
+    public function setBaseCurrency($currencyCode, $type = self::SCOPE_DEFAULT, $scopeId = 0)
+    {
+        $this->setConfigPath('currency/options/base', $currencyCode, $type, $scopeId);
+    }
+
+    public function setDisplayCurrency($currencyCode, $type = self::SCOPE_DEFAULT, $scopeId = 0)
+    {
+        $this->setConfigPath('currency/options/default', $currencyCode, $type, $scopeId);
+    }
+
+    public function setAllowedCurrencies($currencyCode, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('currency/options/allow', $currencyCode, $type, $scopeId);
+    }
+
+
 }
