4fb8d5e9a5e217adf8d0530f0f4647e5a6b20c57
Kenneth Bradley-White <ken@edmondscommerce.co.uk>
2016-11-02 20:51:45 +0000

    Add a load of General Config methods

--- Helper/Config/General.php
+++ Helper/Config/General.php
@@ -1,7 +1,14 @@
 <?php namespace EdmondsCommerce\Migrations\Helper\Config;
 
+use Magento\Config\Model\ResourceModel\Config;
+use Magento\Framework\App\Config\ConfigResource\ConfigInterface;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\StoreManagerInterface;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+
 class General extends AbstractConfig
 {
+
     public function setStoreName($name, $type = self::SCOPE_DEFAULT, $scopeId = 0)
     {
         $this->setConfigPath('general/store_information/name', $name, $type, $scopeId);
@@ -20,6 +27,58 @@ class General extends AbstractConfig
     public function setAllowedCurrencies($currencyCode, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
         $this->setConfigPath('currency/options/allow', $currencyCode, $type, $scopeId);
     }
+
+
+
+    public function getFooterCopyright($type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        return $this->scopeConfig->getValue('design/footer/copyright', $type);
+    }
+
+    public function setFooterCopyright($currencyCode, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('design/footer/copyright', $currencyCode, $type, $scopeId);
+    }
+
+
+
+    public function getWelcomeMessage($type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        return $this->scopeConfig->getValue('design/header/welcome', $type);
+    }
+
+    public function setWelcomeMessage($messageText, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('design/header/welcome', $messageText, $type, $scopeId);
+    }
+
+
+
+    public function getHeadTitle($type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        return $this->scopeConfig->getValue('design/head/default_title', $type);
+    }
+
+    public function setHeadTitle($title, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('design/head/default_title', $title, $type, $scopeId);
+    }
+
+
+
+    public function getHeadDescription($type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        return $this->scopeConfig->getValue('design/head/default_description', $type);
+    }
+
+    public function setHeadDescription($keywords, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('design/head/default_description', $keywords, $type, $scopeId);
+    }
+
+
+
+    public function getHeadKeywords($type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        return $this->scopeConfig->getValue('design/head/default_title', $type);
+    }
+
+    public function setHeadKeywords($keywords, $type = self::SCOPE_DEFAULT, $scopeId = 0) {
+        $this->setConfigPath('design/head/default_title', $keywords, $type, $scopeId);
+    }
+
+
 
 
 }
